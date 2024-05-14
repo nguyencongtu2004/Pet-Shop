3aca7a461f09d9576c4616951dcab04c44be7af6
Tú <22521591@gm.uit.edu.vn>
2024-05-14 10:24:44 +0700

    thêm model

--- app/src/main/java/com/example/petshop/model/Product.kt
+++ app/src/main/java/com/example/petshop/model/Product.kt
@@ -1,17 +1,8 @@
-package com.example.petshop.model;
+package com.example.petshop.model
 
-public class Product {
-    public String name;
-    public String description;
-    public double price;
-    public int quantity;
-
-    public Product(String na, String des, double p, int q){
-        name = na;
-        description = des;
-        price = p;
-        quantity = q;
-    }
-}
-
-
+class Product(
+    name:String,
+    description: String,
+    price: Double,
+    quantity: Int,
+)
